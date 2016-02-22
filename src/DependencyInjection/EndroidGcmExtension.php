acb8e7d6573b5502e06860154dd756d6dd16295f
endroid <info@endroid.nl>
2016-02-22 08:13:12 +0000

    Inject GCM paramters directly in service

--- src/DependencyInjection/EndroidGcmExtension.php
+++ src/DependencyInjection/EndroidGcmExtension.php
@@ -16,15 +16,18 @@ use Symfony\Component\Config\FileLocator;
 
 class EndroidGcmExtension extends Extension
 {
+    /**
+     * {@inheritdoc}
+     */
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $container->setParameter('endroid.gcm.api_key', $config['api_key']);
-        $container->setParameter('endroid.gcm.api_url', $config['api_url']);
-
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        $serviceDefinition = $container->getDefinition('endroid.gcm.client');
+        $serviceDefinition->setArguments(array($config['api_key'], $config['api_url']));
     }
 }
